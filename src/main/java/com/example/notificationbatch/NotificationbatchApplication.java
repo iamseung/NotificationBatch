6be6d418d93ca7acedfac41e72c16233b3bec004
iamseung <seesungseok@naver.com>
2024-08-11 22:32:24 +0900

    init

---
+++ src/main/java/com/example/notificationbatch/NotificationbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificationbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationbatchApplication.class, args);
+	}
+
+}
